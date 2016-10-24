d66bf871f5144d72c492ea6c8a0e9cd983e85c65
Hristo Chernogorov <hristochernogorov@gmail.com>
2016-10-24 12:32:59 +0300

    Move homework in another folder

---
+++ ProgramingCourses/CSharpFundamentals/HomeWork/PrintFullName/PrintName.cs
@@ -0,0 +1,14 @@
+﻿namespace PrintFullName
+{
+    using System;
+
+    public class PrintName
+    {
+        public static void Main()
+        {
+            var firstName = "Hristo";
+            var lastName = "Chernogorov";
+            Console.WriteLine("{0} \n{1}", firstName, lastName);
+        }
+    }
+}
---
+++ ProgramingCourses/CSharpFundamentals/HomeWork/YourAge/AgeMain.cs
@@ -0,0 +1,23 @@
+﻿namespace YourAge
+{
+    using System;
+    public class AgeMain
+    {
+        public static void Main()
+        {
+            var dayOfBirth = DateTime.Parse(Console.ReadLine());
+            var today = DateTime.Today;
+            var yourAgeNow = today.Year - dayOfBirth.Year;
+            if (today < dayOfBirth.AddYears(yourAgeNow)) yourAgeNow--;
+            {
+                Console.WriteLine(yourAgeNow);
+            }
+            var afterTenYears = yourAgeNow + 10;
+            Console.WriteLine(afterTenYears);
+            
+          
+
+
+        }
+    }
+}
