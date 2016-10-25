44071e22311b0722592b1a12bfcd51f0e0a77d43
Hristo Chernogorov <hristochernogorov@gmail.com>
2016-10-25 17:26:57 +0300

    finish some task

---
+++ ProgramingCourses/CSharpFundamentals/HomeWork/DataTypesAndVariables/PrintASCIITable/PrintASCIIMain.cs
@@ -0,0 +1,17 @@
+﻿
+namespace PrintASCIITable
+{
+    using System;
+    public class PrintASCIIMain
+    {
+        public static void Main()
+        {
+            var startSymbol = 33;
+           var endSymbol = 126;
+            for (int i = startSymbol; i <= endSymbol; i++)
+            {
+                Console.Write(((char)i).ToString());
+            }
+        }
+    }
+}
