72e61bf6cd03dec858deac7f0698aa7c479cf9c4
Hristo Chernogorov <hristochernogorov@gmail.com>
2016-11-02 00:03:10 +0200

    is not complete yet

--- ProgramingCourses/CSharpFundamentals/HomeWork/ConditionalStatements/SortTreeNumbers/SortNumbersMain.cs
+++ ProgramingCourses/CSharpFundamentals/HomeWork/ConditionalStatements/SortTreeNumbers/SortNumbersMain.cs
@@ -5,7 +5,15 @@
     {
         public static void Main()
         {
+            var firstNumber = int.Parse(Console.ReadLine());
+            var secondNumber = int.Parse(Console.ReadLine());
+            var thirdNumber = int.Parse(Console.ReadLine());
+            if (firstNumber > secondNumber && firstNumber > thirdNumber ||
+                secondNumber > firstNumber && secondNumber > thirdNumber ||
+                thirdNumber > firstNumber && thirdNumber > secondNumber)
+            {
 
+            }
         }
     }
 }
