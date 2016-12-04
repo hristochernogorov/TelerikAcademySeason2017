431fa9727579cada32a483df26575ac5eacc9e19
Hristo Chernogorov <hristochernogorov@gmail.com>
2016-12-04 16:00:13 +0200

    finish calculate facturial task

--- ProgramingCourses/CSharpFundamentals/HomeWork/Loops/CalculateFacturial/CalculateFacturialMain.cs
+++ ProgramingCourses/CSharpFundamentals/HomeWork/Loops/CalculateFacturial/CalculateFacturialMain.cs
@@ -6,9 +6,20 @@
         public static void Main()
         {
             var N = int.Parse(Console.ReadLine());
-            var x = int.Parse(Console.ReadLine());
+            var x = double.Parse(Console.ReadLine());
+            var facturialN = 1.0;
+            var numXpowerN = 1.0;
+            var sumOfSenquence = 1.0;
+            //S = 1 + 1!/x + 2!/x2 + … + N!/xN
 
-            
+            for (int i = 1; i <= N; i++)
+            {
+                facturialN *= i;
+                numXpowerN *= x;
+                sumOfSenquence += (facturialN / numXpowerN);
+            }
+
+            Console.WriteLine("{0:F5}",sumOfSenquence);
         }
     }
 }
